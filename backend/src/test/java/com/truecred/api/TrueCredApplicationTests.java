90dab2614552f845c6c8e79921d4898306f6d757
roy-abir05 <royabirdhn@gmail.com>
2025-04-07 23:38:48 +0530

    Initial Commit

---
+++ backend/src/test/java/com/truecred/api/TrueCredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truecred.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueCredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
