ad3fdc9205eac70240894d847cf68b2850f15e24
roy-abir05 <royabirdhn@gmail.com>
2025-06-27 18:10:40 +0530

    feat: restructured smart contract

--- contracts/contracts/TrueCred.sol
+++ contracts/contracts/TrueCred.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 contract TrueCred {
     uint256 private _nextTokenId;
 
-    address public owner;
+    address private owner;
 
     constructor() {
         owner = msg.sender;
@@ -16,85 +16,190 @@ contract TrueCred {
         address institution;
         string tokenURI;
         bytes signature;
+        bool isRevoked;
     }
 
     // Only approved institutions can issue certificates
-    mapping(address => bool) public approvedInstitutions;
+    mapping(address => bool) private _approvedInstitutions;
     mapping(uint256 => Certificate) private _certificates;
-    mapping(uint256 => bool) private _revokedCertificates;
-    mapping(address => uint256[]) private _studentToCertificates;
-    mapping(address => uint256[]) private _institutionToCertificates;
 
-    event CertificateIssued(address indexed institution, address indexed student, uint256 indexed tokenId, string tokenURI);
+    event CertificateIssued(
+        uint256 indexed tokenId,
+        address indexed student,
+        address indexed institution
+    );
     event InstitutionApproved(address indexed institution);
     event InstitutionRevoked(address indexed institution);
-    event CertificateRevoked(address indexed institution, uint256 tokenId);
+    event CertificateRevoked(
+        uint256 indexed tokenId,
+        address indexed student,
+        address indexed institution
+    );
 
-    // Modifier to restrict access to approved institutions
-    modifier onlyApprovedInstitution() {
-        require(approvedInstitutions[msg.sender], "Not an approved institution");
-        _;
-    }
-
-    // Modifier to restrict access to only the Deployer of this Smart Contract
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not contract owner");
+        require(msg.sender == owner, "Not the owner");
         _;
     }
 
-    function approveInstitution(address institution) external onlyOwner {
-        approvedInstitutions[institution] = true;
-        emit InstitutionApproved(institution);
+    function approveInstitution(address _institution) external onlyOwner {
+        _approvedInstitutions[_institution] = true;
+        emit InstitutionApproved(_institution);
     }
 
-    function revokeInstitution(address institution) external onlyOwner {
-        approvedInstitutions[institution] = false;
-        emit InstitutionRevoked(institution);
+    function revokeInstitution(address _institution) external onlyOwner {
+        _approvedInstitutions[_institution] = false;
+        emit InstitutionRevoked(_institution);
+    }
+
+    function splitSignature(
+        bytes memory sig
+    ) public pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
+    function recoverSigner(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function verifyIssueSignature(
+        address _student,
+        address _institution,
+        string calldata _tokenURI,
+        bytes calldata _signature
+    ) internal pure returns (bool) {
+        bytes32 messageHash = keccak256(
+            abi.encodePacked("ISSUE", _student, _tokenURI)
+        );
+        bytes32 ethSignedMessageHash = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
+        );
+
+        return recoverSigner(ethSignedMessageHash, _signature) == _institution;
     }
 
     function issueCertificate(
-        address student,
-        string calldata tokenURI,
-        bytes calldata signature
-    ) external onlyApprovedInstitution {
-        require(bytes(tokenURI).length > 0, "Token URI is required");
+        address _student,
+        address _institution,
+        string calldata _tokenURI,
+        bytes calldata _signature
+    ) external {
+        require(
+            _student != address(0) && _approvedInstitutions[_student] == false,
+            "Not a valid student address"
+        );
+        require(bytes(_tokenURI).length > 0, "Token URI is required");
+        require(
+            verifyIssueSignature(_student, _institution, _tokenURI, _signature),
+            "Invalid Signature"
+        );
+        require(
+            _approvedInstitutions[_institution],
+            "Institution not approved"
+        );
 
-        _nextTokenId++;
-        uint256 tokenId = _nextTokenId;
+        ++_nextTokenId;
 
-        _certificates[tokenId] = Certificate({
-            id: tokenId,
-            student: student,
-            institution: msg.sender,
-            tokenURI: tokenURI,
-            signature: signature
+        _certificates[_nextTokenId] = Certificate({
+            id: _nextTokenId,
+            student: _student,
+            institution: _institution,
+            tokenURI: _tokenURI,
+            signature: _signature,
+            isRevoked: false
         });
 
-        _studentToCertificates[student].push(tokenId);
-        _institutionToCertificates[msg.sender].push(tokenId);
-
-        emit CertificateIssued(msg.sender, student, tokenId, tokenURI);
+        emit CertificateIssued(_nextTokenId, _student, _institution);
     }
 
-    function revokeCertificate(uint256 tokenId) external onlyApprovedInstitution {
-        require(_certificates[tokenId].student != address(0), "Invalid token ID");
+    function verifyRevokeSignature(
+        address _student,
+        address _institution,
+        uint256 _tokenId,
+        bytes calldata _signature
+    ) internal pure returns (bool) {
+        bytes32 messageHash = keccak256(
+            abi.encodePacked("REVOKE", _student, _tokenId)
+        );
+        bytes32 ethSignedMessageHash = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
+        );
 
-        _revokedCertificates[tokenId] = true;
-
-        emit CertificateRevoked(msg.sender, tokenId);
+        return recoverSigner(ethSignedMessageHash, _signature) == _institution;
     }
 
-    function getCertificate(uint256 tokenId)
+    function revokeCertificate(
+        address _student,
+        address _institution,
+        uint256 _tokenId,
+        bytes calldata _signature
+    ) external {
+        require(
+            _tokenId <= _nextTokenId &&
+                _certificates[_tokenId].student != address(0),
+            "Invalid token ID"
+        );
+        require(
+            verifyRevokeSignature(_student, _institution, _tokenId, _signature),
+            "Invalid Signature"
+        );
+        require(
+            _approvedInstitutions[_institution],
+            "Institution not approved"
+        );
+        require(
+            _certificates[_tokenId].student == _student,
+            "Student does not own the certificate"
+        );
+        require(
+            _certificates[_tokenId].institution == _institution,
+            "Institution did not issue the certificate"
+        );
+        require(
+            _certificates[_tokenId].isRevoked == false,
+            "Certificate is already Revoked"
+        );
+
+        _certificates[_tokenId].isRevoked = true;
+
+        emit CertificateRevoked(_tokenId, _student, _institution);
+    }
+
+    function getCertificate(
+        uint256 tokenId
+    )
         external
         view
-        returns (address student, string memory tokenURI, bytes memory signature, bool revoked)
+        returns (
+            address student,
+            address institution,
+            string memory tokenURI,
+            bytes memory signature,
+            bool isRevoked
+        )
     {
-        require(_certificates[tokenId].student != address(0), "Invalid token ID");
+        require(
+            tokenId <= _nextTokenId &&
+                _certificates[tokenId].student != address(0),
+            "Invalid token ID"
+        );
         Certificate memory cert = _certificates[tokenId];
-        return (cert.student, cert.tokenURI, cert.signature, _revokedCertificates[tokenId]);
-    }
-
-    function getCertificatesByStudent(address student) external view returns (uint256[] memory) {
-        return _studentToCertificates[student];
+        return (
+            cert.student,
+            cert.institution,
+            cert.tokenURI,
+            cert.signature,
+            cert.isRevoked
+        );
     }
 }
